33790d681dfe7cc8145c517729b9ed735fbee800
Lane <LTPlummer96@gmail.com>
2024-04-23 14:15:43 -0500

    Updated comments for rule 9

--- 9-12/Rule9.cpp
+++ 9-12/Rule9.cpp
@@ -1,7 +1,8 @@
 /**
  * @file Rule9.cpp
  * @author Lane Plummer
- * @brief Catching Exceptions example
+ * @brief   Catching Exceptions example that shows how to handle exceptions thrown instead of ignored or grouped together too 
+ *          vaguely
  * @version 0.1
  * @date 2024-04-10
  * 
