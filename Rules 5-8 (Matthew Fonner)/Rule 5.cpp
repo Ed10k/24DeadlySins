7876188dc5fb0d8c1e7564c0b15d36cddce92f70
mattyf9x <mdfonner@gmail.com>
2024-04-24 13:26:25 -0500

    worked on rule 5

---
+++ Rules 5-8 (Matthew Fonner)/Rule 5.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Rule5.cpp
+ * @author Matthew Fonner
+ * @brief Buffer overrun problems examples.
+ * @version 0.1
+ * @date 2024-04-10
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
